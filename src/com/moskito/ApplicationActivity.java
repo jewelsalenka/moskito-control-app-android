2b37c645fd60b7dbdd8aff90e06dd6b9f0ffdcb2
Olenka Shemshey <jewelsalenka@gmail.com>
2013-08-10 12:46:16 +0300

    Navigation between layout have been added.

--- src/com/moskito/ApplicationActivity.java
+++ src/com/moskito/ApplicationActivity.java
@@ -1,69 +1,45 @@
 package com.moskito;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.*;
 import com.example.moskito_control_app_android.R;
-import com.stub.entity.Application;
-import com.stub.entity.Server;
+import com.stub.entity.*;
 
 /**
  * User: Olenka Shemshey
  * Date: 13.07.13
  */
 public class ApplicationActivity extends Activity{
+    private static Helper mHelper;
     private Application currentApp;
     private TextView noDataView;
+    private SlidingDrawer leftDrawer;
+    private View header;
 
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
-        setContentView(R.layout.application);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null){
-            currentApp = (Application) extras.get(ApplicationsListActivity.KEY_APP);
+        setContentView(R.layout.main);
+        if (mHelper == null){
+            mHelper = new Helper();
         }
+        currentApp = mHelper.getAllApps().get(0);
         obtainView();
-        initializeList();
+        initializeServersList();
+        initializeAppsList();
+        initializeHistoryList();
         updateNoData();
     }
 
     private void obtainView(){
+        leftDrawer = (SlidingDrawer) findViewById(R.id.left_drawer);
         noDataView = (TextView) findViewById(R.id.no_data);
-        View header = findViewById(R.id.header);
-        header.setBackgroundDrawable(getResources().getDrawable(currentApp.getColor().getColorId()));
-        View showApplicationsView = findViewById(R.id.show_applications);
-        showApplicationsView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ApplicationActivity.this, ApplicationsListActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(ApplicationsListActivity.KEY_APP, currentApp);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
-        TextView history = (TextView) findViewById(R.id.history_button);
-        history.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ApplicationActivity.this, HistoryActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(ApplicationsListActivity.KEY_APP, currentApp);
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-        });
+        header = findViewById(R.id.header);
+        updateHead();
     }
 
-    private void initializeList(){
+    private void initializeServersList(){
         final ServerAdapter sAdapter = new ServerAdapter(this, currentApp.getServers());
         ListView lvSimple = (ListView) findViewById(R.id.servers_list);
         lvSimple.setAdapter(sAdapter);
@@ -76,6 +52,44 @@ public class ApplicationActivity extends Activity{
             }
         });
 
+    }
+
+    private void initializeAppsList(){
+        AppAdapter sAdapter = new AppAdapter(this, mHelper.getAllApps());
+        ListView lvSimple = (ListView) findViewById(R.id.app_list);
+        lvSimple.setAdapter(sAdapter);
+        lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                updateAppData(mHelper.getAllApps().get(position));
+                leftDrawer.close();
+            }
+        });
+    }
+
+    private void initializeHistoryList(){
+        HistoryAdapter historyAdapter = new HistoryAdapter(this, currentApp.getHistory());
+        ListView lvSimple = (ListView) findViewById(R.id.history_list);
+        lvSimple.setAdapter(historyAdapter);
+        lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+            }
+        });
+    }
+
+    private void updateAppData(Application app){
+        currentApp = app;
+        updateHead();
+        initializeServersList();
+        initializeHistoryList();
+        updateNoData();
+    }
+
+    private void updateHead(){
+        header.setBackground(getResources().getDrawable(currentApp.getColor().getColorId()));
     }
 
     private void updateNoData(){
