4f3953f331edf3cce9ba8542d5f311641b2571b5
jewelsalenka <jewelsalenka@gmail.com>
2013-10-04 00:41:39 +0300

    Add some GUI changes

--- src/com/moskito/ApplicationActivity.java
+++ src/com/moskito/ApplicationActivity.java
@@ -29,14 +29,14 @@ public class ApplicationActivity extends Activity{
         super.onCreate(bundle);
         setContentView(R.layout.main);
         SlidingMenu menu = new SlidingMenu(this);
-        menu.setMode(SlidingMenu.LEFT);
+        menu.setMode(SlidingMenu.LEFT_RIGHT);
         menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         menu.setShadowWidthRes(R.dimen.shadow_width);
-//        menu.setShadowDrawable(R.drawable.shadow);
         menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
         menu.setFadeDegree(0.35f);
         menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
         menu.setMenu(R.layout.applications);
+        menu.setSecondaryMenu(R.layout.settings);
         obtainView();
         createConnection();
     }
