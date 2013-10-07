bfd6d553cf6065ee127b885dff50602683e28624
jewelsalenka <jewelsalenka@gmail.com>
2013-10-07 22:08:46 +0300

    Add listeners for settings elements.

--- src/com/moskito/ApplicationActivity.java
+++ src/com/moskito/ApplicationActivity.java
@@ -20,14 +20,25 @@ import java.io.IOException;
  */
 public class ApplicationActivity extends Activity{
     private static Helper mHelper;
-    private Application currentApp;
-    private TextView noDataView;
-    private View header;
-    TextView appTitleView;
+    private Application mCurrentApp;
+    private TextView mNoDataView;
+    private View mHeader;
+    private TextView mAppTitleView;
+    private EditText mLogin;
+    private EditText mPassword;
+    private TextView mMinutesUpdateInterval;
 
     public void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.main);
+        initSlidingMenu();
+        obtainView();
+        initSettingsPanel();
+        createConnection();
+        updateHead();
+    }
+
+    private void initSlidingMenu(){
         SlidingMenu menu = new SlidingMenu(this);
         menu.setMode(SlidingMenu.LEFT_RIGHT);
         menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
@@ -38,20 +49,71 @@ public class ApplicationActivity extends Activity{
         menu.setMenu(R.layout.applications);
         menu.setSecondaryMenu(R.layout.settings);
         menu.setSecondaryShadowDrawable(R.drawable.right_shadow);
-        obtainView();
-        createConnection();
     }
 
-    private void obtainView(){
-        noDataView = (TextView) findViewById(R.id.no_data);
-        header = findViewById(R.id.header);
-        appTitleView = (TextView) header.findViewById(R.id.application_title);
-        updateHead();
+    private void initSettingsPanel(){
+        final CheckBox authorization = (CheckBox) findViewById(R.id.checkbox_authorization);
+        final View login = findViewById(R.id.login);
+        final View password = findViewById(R.id.password);
+        final View intervalText = findViewById(R.id.update_interval_text);
+        final RelativeLayout interval = (RelativeLayout) findViewById(R.id.update_interval);
+        final View save = findViewById(R.id.save_button);
+        authorization.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (authorization.isChecked()){
+                    login.setVisibility(View.VISIBLE);
+                    mLogin.setVisibility(View.VISIBLE);
+                    password.setVisibility(View.VISIBLE);
+                    mPassword.setVisibility(View.VISIBLE);
+                    interval.setVisibility(View.VISIBLE);
+                    save.setVisibility(View.VISIBLE);
+                    intervalText.setVisibility(View.VISIBLE);
+                } else {
+                    login.setVisibility(View.INVISIBLE);
+                    mLogin.setVisibility(View.INVISIBLE);
+                    password.setVisibility(View.INVISIBLE);
+                    mPassword.setVisibility(View.INVISIBLE);
+                    interval.setVisibility(View.INVISIBLE);
+                    save.setVisibility(View.INVISIBLE);
+                    intervalText.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
+        Button minusMinutes = (Button) interval.findViewById(R.id.minus_interval);
+        minusMinutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int minutes = Integer.parseInt(mMinutesUpdateInterval.getText().toString());
+                if (minutes > 1) {
+                    minutes--;
+                    mMinutesUpdateInterval.setText(String.valueOf(minutes));
+                }
+            }
+        });
+
+        Button plusMinutes = (Button) interval.findViewById(R.id.plus_interval);
+        plusMinutes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int minutes = Integer.parseInt(mMinutesUpdateInterval.getText().toString());
+                minutes++;
+                mMinutesUpdateInterval.setText(String.valueOf(minutes));
+            }
+        });
+    }
+    private void obtainView(){
+        mNoDataView = (TextView) findViewById(R.id.no_data);
+        mHeader = findViewById(R.id.header);
+        mAppTitleView = (TextView) mHeader.findViewById(R.id.application_title);
+        mLogin = (EditText) findViewById(R.id.login_input);
+        mPassword = (EditText) findViewById(R.id.password_input);
+        mMinutesUpdateInterval = (TextView) findViewById(R.id.interval_minutes);
     }
 
     private void initializeServersList(){
-        final ComponentAdapter sAdapter = new ComponentAdapter(this, currentApp.getComponents());
+        final ComponentAdapter sAdapter = new ComponentAdapter(this, mCurrentApp.getComponents());
         ListView lvSimple = (ListView) findViewById(R.id.servers_list);
         lvSimple.setAdapter(sAdapter);
         lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -79,7 +141,7 @@ public class ApplicationActivity extends Activity{
     }
 
     private void initializeHistoryList(){
-        final HistoryAdapter historyAdapter = new HistoryAdapter(this, currentApp.getHistory());
+        final HistoryAdapter historyAdapter = new HistoryAdapter(this, mCurrentApp.getHistory());
         ListView lvSimple = (ListView) findViewById(R.id.history_list);
         lvSimple.setAdapter(historyAdapter);
         lvSimple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -93,7 +155,7 @@ public class ApplicationActivity extends Activity{
     }
 
     private void updateAppData(Application app){
-        currentApp = app;
+        mCurrentApp = app;
         updateHead();
         initializeServersList();
         initializeHistoryList();
@@ -102,20 +164,20 @@ public class ApplicationActivity extends Activity{
 
     private void updateHead(){
 
-        if (currentApp == null){
-            header.setBackgroundDrawable(getResources().getDrawable(R.color.light_grey));
-            appTitleView.setText("App");
+        if (mCurrentApp == null){
+            mHeader.setBackgroundDrawable(getResources().getDrawable(R.color.light_grey));
+            mAppTitleView.setText("App");
         } else {
-            header.setBackgroundDrawable(getResources().getDrawable(currentApp.getColor().getColorId()));
-            appTitleView.setText(currentApp.getName());
+            mHeader.setBackgroundDrawable(getResources().getDrawable(mCurrentApp.getColor().getColorId()));
+            mAppTitleView.setText(mCurrentApp.getName());
         }
     }
 
     private void updateNoData(){
-        if ((currentApp.getComponents() == null) || (currentApp.getComponents().size() == 0)){
-            noDataView.setVisibility(View.VISIBLE);
+        if ((mCurrentApp.getComponents() == null) || (mCurrentApp.getComponents().size() == 0)){
+            mNoDataView.setVisibility(View.VISIBLE);
         } else {
-            noDataView.setVisibility(View.GONE);
+            mNoDataView.setVisibility(View.GONE);
         }
     }
 
@@ -126,7 +188,7 @@ public class ApplicationActivity extends Activity{
         if (networkInfo != null && networkInfo.isConnected()) {
             new HelperConnection().execute(stringUrl);
         } else {
-            noDataView.setText("No network connection available.");
+            mNoDataView.setText("No network connection available.");
         }
     }
 
@@ -137,11 +199,10 @@ public class ApplicationActivity extends Activity{
                 mHelper = new Helper(urls[0]);
                 return "Connection established";
             } catch (IOException e) {
-                noDataView.setText("Connection failed. URL may be invalid.");
+                mNoDataView.setText("Connection failed. URL may be invalid.");
                 return "Connection failed. URL may be invalid.";
             }
         }
-        // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
             initializeAppsList();
@@ -154,18 +215,16 @@ public class ApplicationActivity extends Activity{
         @Override
         protected String doInBackground(String... urls) {
             try {
-                currentApp.addHistory(mHelper.getHistory(urls[0], currentApp.getName()));
+                mCurrentApp.addHistory(mHelper.getHistory(urls[0], mCurrentApp.getName()));
                 return "Connection established";
             } catch (IOException e) {
-                noDataView.setText("Connection failed. URL may be invalid.");
+                mNoDataView.setText("Connection failed. URL may be invalid.");
                 return "Connection failed. URL may be invalid.";
             }
         }
-        // onPostExecute displays the results of the AsyncTask.
         @Override
         protected void onPostExecute(String result) {
             initializeHistoryList();
-            //updateAppData(mHelper.getAllApps().get(0));
         }
     }
 }
